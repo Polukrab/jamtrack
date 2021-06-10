fe81650b3ce65a4165810a6c6811dcad7ada2f7a
Vitaly Bryukhovetsky <bruhovet@list.ru>
2021-06-10 23:12:43 +0300

    Добавляет файлы с функциями

---
+++ js/autocomplete.js
@@ -0,0 +1,21 @@
+const autocomplete = (search, list) => {
+  // Удалим все лишнее из поискового запроса
+  // Фильтр может быть модифицирован под конкретную задачу, для задачи в тесте дотаточно такого.
+  search.replace('/\W/g', '');
+  // Инициализируем переменную в которую будем писать результат поиска
+  let result = [];
+  // Инициализируем переменную для регулярки
+  let regEx = null;
+  // Ищим пока нужно и есть что искать
+  while (!result.length && search.length) {
+    regEx = new RegExp('^' + search, 'i');
+    // Формируем результат по шаблону поиска
+    result = list.filter(it => (regEx.test(it)));
+    // Если нет совпадений упрощаем критерий поиска
+    if (!result.length) {
+      search = search.slice(0, search.length - 1);
+    }
+  }
+  // Возвращаем результат
+  return result.length > 5 ? (result.slice(0, 5)) : result;
+}
